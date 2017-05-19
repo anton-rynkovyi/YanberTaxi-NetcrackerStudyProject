ad9bff029c70da4fc91ae83a44a13dfd8dea62a5
Anton Rynkovoy <anton.rynkovoy@gmail.com>
2017-05-19 22:27:46 +0300

    fix MainPage class

--- src/main/java/com/netcracker/project/study/vaadin/test/ui/MainPage.java
+++ src/main/java/com/netcracker/project/study/vaadin/test/ui/MainPage.java
@@ -1,59 +1,29 @@
 package com.netcracker.project.study.vaadin.test.ui;
-import com.netcracker.project.study.model.client.Client;
-import com.netcracker.project.study.model.driver.Driver;
-import com.netcracker.project.study.persistence.facade.impl.PersistenceFacade;
+
+
 import com.netcracker.project.study.vaadin.test.components.HeaderMenu;
-import com.netcracker.project.study.vaadin.test.view.AdminView;
 import com.vaadin.annotations.Theme;
-import com.vaadin.data.ValueProvider;
-import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 
-import java.math.BigInteger;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.vaadin.ui.renderers.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Theme("valo")
-@SpringUI(path = "s")
+@SpringUI(path = "")
 public class MainPage extends UI {
 
     @Autowired
     private HeaderMenu headerMenu;
 
-    @Autowired
-    PersistenceFacade facade;
-
-    @Autowired
-    DriversCreatePopUp driversCreatePopUp;
-
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        VerticalLayout layoutContent = genLayoutContent();
-        VerticalLayout viewLayout = new VerticalLayout();
-        setContent(layoutContent);
-        layoutContent.addComponents(headerMenu, viewLayout);
-
-        Navigator navigator = new Navigator(this, viewLayout);
-        navigator.addView("admin", AdminView.class);
-    }
-
-
-
-    private VerticalLayout genLayoutContent(){
         VerticalLayout layoutContent = new VerticalLayout();
-        layoutContent.setSizeFull();
-        layoutContent.setMargin(false);
-        layoutContent.setSpacing(false);
-        return layoutContent;
+        setContent(layoutContent);
+        layoutContent.addComponents(headerMenu);
+
     }
 
 }
