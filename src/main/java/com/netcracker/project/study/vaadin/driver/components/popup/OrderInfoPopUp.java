04233925c6d1ec0737057e154dcb4601ba3c6c4b
MathMark <vadimmartsun@gmail.com>
2017-06-02 03:42:28 +0300

    Implement the opportunity to see all information about an order, included the route.

--- src/main/java/com/netcracker/project/study/vaadin/driver/components/popup/OrderInfoPopUp.java
+++ src/main/java/com/netcracker/project/study/vaadin/driver/components/popup/OrderInfoPopUp.java
@@ -1,19 +1,26 @@
 package com.netcracker.project.study.vaadin.driver.components.popup;
 
+import com.netcracker.project.study.model.order.route.Route;
 import com.netcracker.project.study.services.impl.OrderServiceImpl;
 import com.netcracker.project.study.vaadin.driver.pojos.OrderInfo;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringComponent
 public class OrderInfoPopUp extends VerticalLayout {
 
     private OrderInfo orderInfo;
 
-    VerticalLayout rootLayout;
+    HorizontalLayout rootLayout;
     @Autowired private OrderServiceImpl orderService;
 
     public void init(OrderInfo orderInfo) {
@@ -25,20 +32,51 @@ public class OrderInfoPopUp extends VerticalLayout {
     }
 
     private void initRootLayout(){
-        rootLayout = new VerticalLayout();
+        rootLayout = new HorizontalLayout();
         rootLayout.setSizeFull();
         rootLayout.setSpacing(true);
         rootLayout.setMargin(true);
     }
 
     private void initOrderInfoLayout(){
-        VerticalLayout verticalLayout = new VerticalLayout();
+        VerticalLayout orderLayout = new VerticalLayout();
         Label driverId = new Label("<b>Driver: </b>"+ orderInfo.getDriverName(), ContentMode.HTML);
+        driverId.setIcon(FontAwesome.MALE);
         Label clientId = new Label("<b>Client: </b>" + orderInfo.getClientName(), ContentMode.HTML);
+        clientId.setIcon(FontAwesome.MALE);
         Label status = new Label("<b>Status: </b>" + orderInfo.getStatus(), ContentMode.HTML);
+        status.setIcon(FontAwesome.ROUBLE);
         Label cost = new Label("<b>Cost: </b>" + orderInfo.getCost(), ContentMode.HTML);
+        cost.setIcon(FontAwesome.DOLLAR);
         Label distance = new Label("<b>Distance: </b>" + orderInfo.getDistance(), ContentMode.HTML);
-        verticalLayout.addComponents(driverId,clientId,status,cost,distance);
-        rootLayout.addComponent(verticalLayout);
+        distance.setIcon(FontAwesome.ARROWS_H);
+        orderLayout.addComponents(driverId,clientId,status,cost,distance);
+
+        Panel orderPanel = new Panel("Order", orderLayout);
+        orderPanel.setIcon(FontAwesome.MOBILE);
+        orderPanel.setWidth(400, Unit.PIXELS);
+
+        VerticalLayout routeLayout = getRoutesLayout();
+        Panel routePanel = new Panel("Route", routeLayout);
+        routePanel.setIcon(FontAwesome.ROAD);
+        routePanel.setWidth(400, Unit.PIXELS);
+
+        rootLayout.addComponent(orderPanel);
+        rootLayout.addComponent(routePanel);
+    }
+
+    private VerticalLayout getRoutesLayout(){
+        VerticalLayout routeLayout= new VerticalLayout();
+        List<Route> routes = orderService.getRoutes(orderInfo.getObjectId());
+
+        int i = 0;
+        for(Route route:routes){
+            Label label = new Label("<b>Address " + i + ": </b>"+ route.getCheckPoint(), ContentMode.HTML);
+            label.setIcon(FontAwesome.MAP_MARKER);
+            routeLayout.addComponent(label);
+            i++;
+        }
+
+        return routeLayout;
     }
 }
