2d2a073ee95b7baac4958956f308ae9d266c4747
Наталия <natadancer1@gmail.com>
2017-06-14 02:40:48 +0300

    create user packge and changed taxo_db.sql

---
+++ src/main/java/com/netcracker/project/study/model/user/UserRoleList.java
@@ -0,0 +1,13 @@
+package com.netcracker.project.study.model.user;
+
+import java.math.BigInteger;
+
+public interface UserRoleList {
+
+    BigInteger ADMIN = BigInteger.valueOf(11);
+
+    BigInteger DRIVER = BigInteger.valueOf(12);
+
+    BigInteger CLIENT = BigInteger.valueOf(13);
+
+}
