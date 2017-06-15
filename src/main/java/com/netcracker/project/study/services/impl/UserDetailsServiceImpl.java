a7ea24bf921545c5a0d8335c1620e7e538a2a214
MathMark <vadimmartsun@gmail.com>
2017-06-15 23:58:47 +0300

    restore lacking methods.

--- src/main/java/com/netcracker/project/study/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/netcracker/project/study/services/impl/UserDetailsServiceImpl.java
@@ -10,9 +10,15 @@ import com.netcracker.project.study.model.driver.DriverAttr;
 import com.netcracker.project.study.model.driver.DriverStatusList;
 import com.netcracker.project.study.persistence.facade.impl.PersistenceFacade;
 import com.netcracker.project.study.vaadin.driver.components.views.OrdersViewForDrivers;
+import com.sun.istack.NotNull;
 import com.vaadin.spring.annotation.ViewScope;
 import org.apache.xpath.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.security.access.method.P;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -25,6 +31,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 @Service
@@ -32,6 +42,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     PersistenceFacade persistenceFacade;
+
+    private JdbcTemplate jdbcTemplate;
+
+    public static final String CREATE_USERS = ""+
+            "INSERT INTO USERS" +
+            "(object_id, login, password,role)" +
+            " VALUES(?,?,?,?,?)";
+
+    public static final String SELECT_USERS = ""+
+            "SELECT * " +
+            "FROM USERS " +
+            "WHERE login = ? and password = ?";
 
     @Override
     @Transactional(readOnly = true)
@@ -72,6 +94,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return user;
     }
 
+    public com.netcracker.project.study.model.user.User createUser(com.netcracker.project.study.model.user.User user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps =
+                        connection.prepareStatement(CREATE_USERS, new String[] {"user_id"});
+                int i = 0;
+                ps.setString(++i, user.getObjectId().toString());
+                ps.setString(++i, user.getLogin());
+                ps.setString(++i, user.getPassword());
+                ps.setString(++i, user.getRole());
+                return ps;
+            }
+        }, keyHolder);
+        user.setUserId(BigInteger.valueOf(keyHolder.getKey().longValue()));
+        return user;
+    }
+
     public boolean hasRole(String role) {
         Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>)
                 SecurityContextHolder.getContext().getAuthentication().getAuthorities();
@@ -97,4 +137,27 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
         return null;
     }
+
+    public com.netcracker.project.study.model.user.User selectUser(String login, String password) {
+        com.netcracker.project.study.model.user.User user = jdbcTemplate.queryForObject(SELECT_USERS,new Object[] {login,password},rowMapper);
+        return user;
+    }
+
+    @NotNull
+    private RowMapper<com.netcracker.project.study.model.user.User> rowMapper = new RowMapper<com.netcracker.project.study.model.user.User>() {
+        @Override
+        public com.netcracker.project.study.model.user.User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            com.netcracker.project.study.model.user.User user = new com.netcracker.project.study.model.user.User();
+            user.setUserId(rs.getBigDecimal("user_id").toBigInteger());
+            user.setObjectId(rs.getBigDecimal("object_id").toBigInteger());
+            user.setLogin(rs.getString("login"));
+            user.setPassword(rs.getString("password"));
+            user.setRole(rs.getString("role"));
+            return user;
+        }
+    };
+
+
+
+
 }
