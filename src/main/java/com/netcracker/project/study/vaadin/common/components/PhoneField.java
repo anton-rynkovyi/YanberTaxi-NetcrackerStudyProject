15c289b37fa99c390bdf5003f6724800ccd45dda
Eduard Belchik <stor16@gmail.com>
2017-07-04 02:35:23 +0300

    fix phone number field

--- src/main/java/com/netcracker/project/study/vaadin/common/components/PhoneField.java
+++ src/main/java/com/netcracker/project/study/vaadin/common/components/PhoneField.java
@@ -18,28 +18,37 @@ public class PhoneField extends TextField implements HasValue.ValueChangeListene
         setValueChangeMode(ValueChangeMode.EAGER);
         addValueChangeListener(this);
         setMaxLength(14);
-        //setValue("+38");
-        //setCursorPosition(3);
+        setPlaceholder("(000)000-00-00");
     }
 
     @Override
     public void valueChange(ValueChangeEvent event) {
-        str += getValue();
-      /*  if (getValue().length() == 2) {
-            setValue("+38()");
-            setCursorPosition(4);
+        if (getValue().length() >= 0 && !getValue().contains("(")){
+            str = "(" + getValue();
+            setValue(str);
+            isDelete = false;
         }
-        if (getValue().length() == 8) {
-            setCursorPosition(8);
+        if (getValue().length() > 4 && !getValue().contains(")")){
+            str = getValue().substring(0,4) + ")" + getValue().substring(4);
+            setValue(str);
+            isDelete = false;
         }
-        if (getValue().length() == 11) {
-            setValue(getValue() + "-");
-        }*/
-
-        if (getValue().length() == 3) {
-            setValue("(" + getValue() + ")");
-        } else if (getValue().length() == 8 || getValue().length() == 11) {
-            setValue(getValue() + "-");
+        if (getValue().length() == 8 && !getValue().contains("-") && !isDelete){
+            str = getValue() + "-";
+            setValue(str);
+        }
+        if (getValue().length() == 9 && !getValue().substring(8).contains("-")){
+            str = getValue().substring(0,8) + "-" + getValue().substring(8);
+            setValue(str);
+        }
+        if (getValue().length() == 11 && !isDelete){
+            str = getValue() + "-";
+            setValue(str);
+            isDelete = true;
+        }
+        if (getValue().length() == 12 && !getValue().substring(11).contains("-")){
+            str = getValue().substring(0,11) + "-" + getValue().substring(11);
+            setValue(str);
         }
     }
 }
